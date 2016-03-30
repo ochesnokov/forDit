1aca5b3581f1ac680706221370ec629438814590
Чесноков Олег <ochesnokov@diasoft.ru>
2016-03-30 15:03:21 +0300

    -

--- src/main/java/beans/ClientOrderBean.java
+++ src/main/java/beans/ClientOrderBean.java
@@ -13,6 +13,7 @@ import javax.persistence.Table;
 @Table(name = "dbo.tClientOrder")
 public class ClientOrderBean {
 
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "ClientOrderId")
